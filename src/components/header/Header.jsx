2aec3e7afc34d4d58a347cab1cdaf55ef749e306
MAlexGG <alexandragalarzag@gmail.com>
2026-05-23 20:20:31 +0200

    conditional rendering short logo if it is mobile

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,21 @@
 import styles from './Header.module.css'
 import { Link, NavLink } from 'react-router'
 import logo from '../../assets/img/logo.jpg'
+import shortLogo from '../../assets/img/logo_short.jpg'
+import { useResponsive } from '../../hooks/useResponsive';
 
 function Header() {
+
+  const { isMobile } = useResponsive();
+
   return (
     <header className={styles.ctHeader}>
-        <img src={logo} alt="logo" className={styles.imgLogo}/>
-        <nav className={styles.ctNav}>
-            <NavLink to={"/"} className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link }>Home</NavLink>
-            <NavLink to={"/dashboard"} className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link }>Dashboard</NavLink>
+      {
+        isMobile ? <img src={shortLogo} alt="logo" className={styles.imgLogo}/> : <img src={logo} alt="logo" className={styles.imgLogo}/>
+      }
+      <nav className={styles.ctNav}>
+          <NavLink to={"/"} className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link }>Home</NavLink>
+          <NavLink to={"/dashboard"} className={({ isActive }) => isActive ? `${styles.link} ${styles.active}` : styles.link }>Dashboard</NavLink>
         </nav>
     </header>
   )
