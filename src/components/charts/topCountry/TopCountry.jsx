78f4d05c6568c73ac0523888559928b34499e7eb
MAlexGG <alexandragalarzag@gmail.com>
2026-05-24 13:52:40 +0200

    affected countries chart only in desktop

---
+++ src/components/charts/topCountry/TopCountry.jsx
@@ -0,0 +1,9 @@
+import styles from './TopCountry.module.css'
+
+function TopCountry() {
+  return (
+    <div>TopCountry</div>
+  )
+}
+
+export default TopCountry
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,24 @@
 import styles from './Dashboard.module.css'
 import GlobalData from "../../components/charts/globalData/GlobalData"
+import { useResponsive } from '../../hooks/useResponsive'
 
 function Dashboard() {
+
+  const { isDesktop } = useResponsive();
+
+
   return (
     <>
       <div className={styles.ctDashboard}>
         <h2>Global Analytics Dashboard</h2>
         <p>Real-time epidemiological surveillance and data modeling.</p>
+        {
+          isDesktop &&
+          <div className={styles.ctChartTotalCountries}>
+            <h6>Affected Countries</h6>
+            <p>231</p>
+          </div>
+        }
       </div>
       <GlobalData/>
     </>
